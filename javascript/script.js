7311bc6597a3752d45bac35d5ea60c7b47e9b699
Marino Pušić <marino.pusic.5@gmail.com>
2024-09-22 14:56:17 +0200

    some dom

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+/*document.getElementById("promijeniNaslov").addEventListener("click", function() {
+    const naslovElement = document.getElementById("naslov");
+    const currentColor = window.getComputedStyle(naslovElement).color;
+
+    if (currentColor === 'rgb(0, 0, 0)') { // 'black' u RGB formatu
+        naslovElement.style.color = "red";
+        console.log('red');
+    } else if (currentColor === 'rgb(255, 0, 0)') { // 'red' u RGB formatu
+        naslovElement.style.color = "black";
+        console.log('black');
+    }
+});
+
+const naslov = document.getElementById("naslov");
+console.log(naslov.parentNode); // Prikazuje roditelja
+console.log(naslov.childNodes);  // Prikazuje decu (tekst i druge elemente)
+
+
+const noviH1 = document.createElement("h1");
+noviH1.textContent = 'Ovo je primjer teksta';
+document.body.appendChild(noviH1);
+
+const paragraf = document.createElement('p');
+paragraf.textContent = 'Ovo je paragraf ispod h1 naslova'
+
+naslov.appendChild(paragraf);
+
+const naslovElement = document.getElementById('promijeniNaslov');
+
+naslovElement.addEventListener('mouseover',function(){
+    naslovElement.style.background = '#fffff';
+});
+naslovElement.addEventListener('mouseleave',function(){
+    naslovElement.style.display = 'none'; 
+});*/
+
+/*const forma = document.getElementById('mojaForma');
+forma.addEventListener('submit',function(e){
+    e.preventDefault;
+    const ime = document.getElementById('ime').value;
+    const sifra = document.getElementById('password').value;
+    console.log('Pozdrav' + ' ' +  ime );
+    console.log(sifra);
+})*/
+
+
+
+
+
+
+
+
